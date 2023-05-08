4f5c05b5566837564c1d881c35c617fe1d3f89aa
sachin7777777 <122624349+sachin7777777@users.noreply.github.com>
2023-05-08 11:44:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Detail from "./Components/FuncCompo"
+
+function App() {
+  return (
+    <div>
+      <Detail/>
+    </div>
+  );
+}
+
+export default App;
